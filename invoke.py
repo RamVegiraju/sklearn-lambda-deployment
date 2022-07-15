df930ac2abd500812bf11da4919a1765df772159
Ram Vegiraju <48113975+RamVegiraju@users.noreply.github.com>
2022-07-15 00:18:08 -0400

    Update invoke.py

--- invoke.py
+++ invoke.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "https://yzxq6wjbydmz6wcfrzoyalpnke0eixed.lambda-url.us-east-1.on.aws/"
+url = "Enter function URL"
 
 data = '{"data": [[5,5,3,2], [2,4,3,5]]}'
 resp = requests.post(url, json = data)
